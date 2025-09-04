de032e1bbae91fef774763eaf540ca9acbdcd94d
Jea Hyun Lee <jealee44@gmail.com>
2025-09-03 21:14:15 -0400

    status window component continued

--- src/components/StatusWindow/StatusWindow.tsx
+++ src/components/StatusWindow/StatusWindow.tsx
@@ -1,9 +1,14 @@
 import {motion} from 'motion/react'
 
 const StatusWindow = () => {
+    const stats = {
+        name: "Jea Lee",
+        level: 2,
+        class: "Full-Stack Developer",
+    }
 
     return (
-        <div>
+        <div className="bg-gradient-to-br from-[rgba(10,8,24,0.95)] to-[rgba(26,18,48,0.95)] border-2 border-solid border-[rgba(163,230,255,0.5)]">
             Status Window Component
         </div>
     )

999ad97d2983c035f5bd9c006da2a395ed7def75
Jea Hyun Lee <jealee44@gmail.com>
2025-09-11 02:00:32 -0400

    navbar design and placement finished, added a projects placeholder to make sure navbar is sticky

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import StatusWindow from './components/StatusWindow/StatusWindow';
 import Avatar from './components/StatusWindow/Avatar';
+import NavBar from './components/Nav/NavBar';
 
 
 export default function App() {
  return (
-    <main className="relative z-10 mx-auto max-w-[1200px] px-4 py-12">
+    <main className="relative z-10 mx-auto max-w-[1980px] px-4 py-12">
+      <div>
+        <NavBar />
+      </div>
       <div className="grid items-start gap-8 lg:grid-cols-[minmax(0,1fr)_420px]">
         <div>
           <StatusWindow />
@@ -12,6 +16,9 @@ export default function App() {
         <div className="justify-self-center">
           <Avatar className="w-[260px] sm:w-[320px] lg:w-[420px]" />
         </div>
+      </div>
+      <div className="bg-(--color-dark) h-screen text-white" >
+        Projects
       </div>
     </main>
   );
--- src/components/Nav/NavBar.tsx
+++ src/components/Nav/NavBar.tsx
@@ -1,3 +1,43 @@
 import {useState} from 'react';
 import { MdMenu, MdEmail } from "react-icons/md";
 import { SiLinkedin, SiGithub } from "react-icons/si";
+
+
+const NAV = [
+    { href: "#about", label: "About"},
+    { href: "#projects", label: "Projects" },
+    { href: "#skills", label: "Skills"},
+    { href: "#resume", label: "Resume"},
+] as const;
+
+
+const SOCIAL = [
+    {href: "https://github.com/jealee44", label: "GitHub", Icon: SiGithub },
+    {href: "https://linkedin.com/in/jealee44", label: "LinkedIn", Icon: SiLinkedin },
+    {href: "mailto:jealee44@gmail.com", label: "Email", Icon: MdEmail },
+] as const;
+
+export default function NavBar() {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <header className="bg-dark/40 backdrop-blur-md text-cyan-light font-family-heading border-b ">
+            <nav className="px-4 flex justify-between mx-auto max-w-screen h-18 items-center ">
+                <a href="#about">JEA LEE</a>
+
+                <ul className="hidden md:flex gap-6 items-center">
+                    <li><a href="#about">About</a></li>
+                    <li><a href="projects">Projects</a></li>
+                    <li><a href="skills">Skills</a></li>
+                    <li><a href="resume">Resume</a></li>
+                </ul>
+
+                <div>
+                    icons
+                    icons
+                    icons
+                </div>
+            </nav>
+        </header>
+    );
+}
